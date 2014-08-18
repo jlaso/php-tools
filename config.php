cfe1a9f185b67c70e80fb4de696e5e26879803b7
Patrick JL Laso <wld1373@gmail.com>
2014-08-18 15:31:23 +0200

    - added example and refactor of classes, added constants to config.php

--- config.php
+++ config.php
@@ -1,4 +1,10 @@
 <?php
 
+// constants for DbTools class
+define("DB_HOST", "127.0.0.1");
+define("DB_USER", "root");
+define("DB_PASSWORD", "root");
+define("DB_NAME", "test");
 
+// constants for Session class
 define("APP_COOKIE_EXPIRA", 1); // days to expire cookie of remember me
--- php-tools.php
+++ php-tools.php
@@ -1,14 +1,5 @@
 <?php
 
-   error_reporting( E_ALL & ~E_NOTICE & ~E_DEPRECATED );
-
-   define ("_ROOT_", dirname(__FILE__));
-   define ("_SERVER_", "http://".$_SERVER['SERVER_NAME'].'/'.basename(_ROOT_)."/");
-
-   @define ("__DIR__",dirname(__FILE__));
-   require_once __DIR__.'/config.php';
-
-
 /**
  * Class DbTools
  *
